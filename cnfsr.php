5258a03989ff5040308e0758945ae5a43f3ec18a
Ben Ward <benmward41@gmail.com>
2015-05-23 14:55:14 -0400

    i forgot to take out the goddamn farmers song...now fixedgit add -A

--- cnfsr.php
+++ cnfsr.php
@@ -7,48 +7,45 @@
 	<meta name="description" content="Ben's personal website.">
 	<meta name="author" content="Benjamin M Ward">
 
-  <audio autoplay>
-    <source src="sound/farmers.mp3" type="audio/mpeg">
-    </audio>
 
-    <!--[if lte IE 8]><script src="css/ie/html5shiv.js"></script><![endif]-->
-    <!--[if lte IE 8]><script src="assets/css/ie/html5shiv.js"></script><![endif]-->
-    <link rel="stylesheet" href="assets/css/main.css" />
-    <!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie/v8.css" /><![endif]-->
-    <!--[if lte IE 9]><link rel="stylesheet" href="assets/css/ie/v9.css" /><![endif]-->
-    <!--[if lte IE 8]><script src="assets/css/ie/respond.min.js"></script><![endif]-->
+  <!--[if lte IE 8]><script src="css/ie/html5shiv.js"></script><![endif]-->
+  <!--[if lte IE 8]><script src="assets/css/ie/html5shiv.js"></script><![endif]-->
+  <link rel="stylesheet" href="assets/css/main.css" />
+  <!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie/v8.css" /><![endif]-->
+  <!--[if lte IE 9]><link rel="stylesheet" href="assets/css/ie/v9.css" /><![endif]-->
+  <!--[if lte IE 8]><script src="assets/css/ie/respond.min.js"></script><![endif]-->
 
-    <!-- Piwik -->
-    <script type="text/javascript">
-      var _paq = _paq || [];
-      _paq.push(['trackPageView']);
-      _paq.push(['enableLinkTracking']);
-      (function() {
-       var u="//benmward.com/analytics/";
-       _paq.push(['setTrackerUrl', u+'piwik.php']);
-       _paq.push(['setSiteId', 1]);
-       var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-       g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
-     })();
-   </script>
-   <noscript><p><img src="//benmward.com/analytics/piwik.php?idsite=1" style="border:0;" alt="" /></p></noscript>
-   <!-- End Piwik Code -->
+  <!-- Piwik -->
+  <script type="text/javascript">
+    var _paq = _paq || [];
+    _paq.push(['trackPageView']);
+    _paq.push(['enableLinkTracking']);
+    (function() {
+     var u="//benmward.com/analytics/";
+     _paq.push(['setTrackerUrl', u+'piwik.php']);
+     _paq.push(['setSiteId', 1]);
+     var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+     g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
+   })();
+ </script>
+ <noscript><p><img src="//benmward.com/analytics/piwik.php?idsite=1" style="border:0;" alt="" /></p></noscript>
+ <!-- End Piwik Code -->
 
- </head>
- <body class="landing">
+</head>
+<body class="landing">
 
-   <!-- Page Wrapper -->
-   <!-- Header -->
-   <header id="header" class="alt">
-    <nav id="nav">
-     <ul>
-      <li class="special">
-       <a href="#menu" class="menuToggle"><span>Projects</span></a>
-       <div id="menu">
-        <?php include 'menu.php';?>
-      </div>
-    </li>
-  </ul>
+ <!-- Page Wrapper -->
+ <!-- Header -->
+ <header id="header" class="alt">
+  <nav id="nav">
+   <ul>
+    <li class="special">
+     <a href="#menu" class="menuToggle"><span>Projects</span></a>
+     <div id="menu">
+      <?php include 'menu.php';?>
+    </div>
+  </li>
+</ul>
 </nav>
 </header>
 
@@ -57,9 +54,10 @@
   <div class="inner">
    <h2>CNFSR</h2>
    <div class="container">
-      <p>A hackathon submission from VT Hacks Spring 2014. <a href="http://challengepost.com/software/cnfsr-the-college-confessions-website">CLICK HERE TO VIEW THE PROJECT ON WWW.CHALLENGEPOST.COM</a></p>
-     <p><a href="http://challengepost.com/software/cnfsr-the-college-confessions-website">
-     <img src="http://challengepost-s3-challengepost.netdna-ssl.com/photos/production/software_photos/000/124/629/datas/gallery.jpg" alt="CNFSR">
+    <p>A hackathon submission from VT Hacks Spring 2014.
+      <br><a href="http://challengepost.com/software/cnfsr-the-college-confessions-website">CLICK HERE TO VIEW THE PROJECT ON WWW.CHALLENGEPOST.COM</a></p>
+      <p><a href="http://challengepost.com/software/cnfsr-the-college-confessions-website">
+       <img src="http://challengepost-s3-challengepost.netdna-ssl.com/photos/production/software_photos/000/124/629/datas/gallery.jpg" alt="CNFSR">
      </a></p>
    </div>
  </div>
