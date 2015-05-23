130667bbc567d984bdfb6f6e7d5a7b3ad90fbd1c
Ben Ward <benmward41@gmail.com>
2015-05-23 10:09:05 -0400

    testing php again

--- menu.php
+++ menu.php
@@ -1,8 +1,10 @@
 <?php
-echo '<li><a href="http://www.benmward.com">benmward.com</a></li> -
-<br> -
-<li><a href="musmass.html">Musical Massacre</a></li> -
-<li><a href="glyde.html">Glyde</a></li> -
-<li><a href="jumpydeer.html">Jumpy Deer <i class="fa fa-android"></i></a></li> -
-<li><a href="fox.html">Fox (Joke)</a></li>';
+echo '<ul>
+<li><a href="http://www.benmward.com">benmward.com</a></li>
+<br>
+<li><a href="musmass.html">Musical Massacre</a></li>
+<li><a href="glyde.html">Glyde</a></li>
+<li><a href="jumpydeer.html">Jumpy Deer <i class="fa fa-android"></i></a></li>
+<li><a href="fox.html">Fox (Joke)</a></li>
+<ul>';
 ?>
