ede2bc95d2307e96f1adb6def3ab9256d2914366
Ben Ward <benmward41@gmail.com>
2015-05-23 09:59:03 -0400

    testing php

---
+++ menu.php
@@ -0,0 +1,8 @@
+<?php
+echo '<li><a href="http://www.benmward.com">benmward.com</a></li> -
+<br> -
+<li><a href="musmass.html">Musical Massacre</a></li> -
+<li><a href="glyde.html">Glyde</a></li> -
+<li><a href="jumpydeer.html">Jumpy Deer <i class="fa fa-android"></i></a></li> -
+<li><a href="fox.html">Fox (Joke)</a></li>';
+?>
