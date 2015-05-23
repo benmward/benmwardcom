9936258bec89bd01ee045f3b04440a0b3b2bb7cd
Ben Ward <benmward41@gmail.com>
2015-05-23 10:45:45 -0400

    new menu order

--- menu.php
+++ menu.php
@@ -3,9 +3,9 @@ echo '
 						<ul>
 							<li><a href="http://www.benmward.com">benmward.com</a></li>
 							<br>
-							<li><a href="musmass.php">Musical Massacre</a></li>
 							<li><a href="glyde.php">Glyde</a></li>
 							<li><a href="jumpydeer.php">Jumpy Deer <i class="fa fa-android"></i></a></li>
+							<li><a href="musmass.php">Musical Massacre</a></li>
 							<li><a href="fox.php">Fox (Joke)</a></li>
 						</ul>
 ';
