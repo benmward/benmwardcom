aae1a570a203f163f61a36c8387c8826defcf070
Ben Ward <benmward41@gmail.com>
2015-05-24 09:35:33 -0400

    reorder menu

--- menu.php
+++ menu.php
@@ -3,14 +3,14 @@ echo '
 						<ul>
 							<li><a href="http://www.benmward.com">Back to benmward.com</a></li>
 							<hr>
-							<li><a href="glyde.php">Glyde</a></li>
 							<li><a href="jumpydeer.php">Jumpy Deer <i class="fa fa-android"></i></a></li>
 							<li><a href="musmass.php">Musical Massacre</a></li>
-							<li><a href="cataclysm.php">Cataclysm</a></li>
+							<li><a href="glyde.php">Glyde</a></li>
 							<li><a href="cnfsr.php">CNFSR</a></li>
 							<li><a href="generosime.php">Generosi.me</a></li>
-							<li><a href="tabulater.php">Tabulater</a></li>
 							<li><a href="terrorborn.php">Terrorborn</a></li>
+							<li><a href="cataclysm.php">Cataclysm</a></li>
+							<li><a href="tabulater.php">Tabulater</a></li>
 							<hr>
 							<li><a href="hackathons.php">Hackathons</a></li>
 						</ul>
