9d0f39c1a071f2fc3b1c37d37006a62351b354f1
Ben Ward <benmward41@gmail.com>
2015-06-19 17:52:43 -0400

    imprisioned improvements

--- imprisoned.php
+++ imprisoned.php
@@ -61,7 +61,7 @@
 						<p>Your browser doesn't support HTML5 canvas.</p>
 					</canvas>
 				</div>
-				<script type="text/javascript" src="imprisoned/Imprisoned.js?IDCBC=1242579729"></script>
+				<script type="text/javascript" src="imprisoned/Imprisoned.js?HJPZB=1394338246"></script>
 
 				<!-- / PROJECT END -->
 				<!-- Copyright Signature -->
