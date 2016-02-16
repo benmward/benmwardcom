da50213b61a4d7020f56e1f4a4aff4c08a762612
Ben Ward <benmward41@gmail.com>
2016-02-16 09:25:18 -0500

    cdt

--- cigarettesdiarrheatrash.php
+++ cigarettesdiarrheatrash.php
@@ -52,7 +52,7 @@
 		<div class="inner">
 			<!-- PROJECT START -->
 			<div class="container">
-				<iframe src="https://docs.google.com/document/d/1snfKzDqosjOptdPOBoaRxHlyVMIGiOpw3gI_YPU2uBI/pub?embedded=true" width="72%" height="720px" frameborder="1"></iframe>
+				<iframe src="https://docs.google.com/document/d/1snfKzDqosjOptdPOBoaRxHlyVMIGiOpw3gI_YPU2uBI/pub?embedded=true" width="66%" height="666px" frameborder="1"></iframe>
 			</div>
 			<!-- / PROJECT END -->
 			<!-- Copyright Signature -->
