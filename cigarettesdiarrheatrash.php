3a6275d3017922a51bda6111f9f5ac99f623e742
Ben Ward <benmward41@gmail.com>
2016-02-15 23:19:18 -0500

    cdt

---
+++ cigarettesdiarrheatrash.php
@@ -0,0 +1,77 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+	<title>Benjamin M. Ward</title>
+	<link rel="icon" href=bmw.ico />
+	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+	<meta name="description" content="Benjamin M. Ward's personal website.">
+	<meta name="author" content="Benjamin M Ward">
+	<!--[if lte IE 8]><script src="css/ie/html5shiv.js"></script><![endif]-->
+	<!--[if lte IE 8]><script src="assets/css/ie/html5shiv.js"></script><![endif]-->
+	<link rel="stylesheet" href="assets/css/main.css" />
+	<!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie/v8.css" /><![endif]-->
+	<!--[if lte IE 9]><link rel="stylesheet" href="assets/css/ie/v9.css" /><![endif]-->
+	<!--[if lte IE 8]><script src="assets/css/ie/respond.min.js"></script><![endif]-->
+
+	<!-- Piwik -->
+	<script type="text/javascript">
+		var _paq = _paq || [];
+		_paq.push(['trackPageView']);
+		_paq.push(['enableLinkTracking']);
+		(function() {
+			var u="//benmward.com/analytics/";
+			_paq.push(['setTrackerUrl', u+'piwik.php']);
+			_paq.push(['setSiteId', 1]);
+			var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+			g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
+		})();
+	</script>
+	<noscript><p><img src="//benmward.com/analytics/piwik.php?idsite=1" style="border:0;" alt="" /></p></noscript>
+	<!-- End Piwik Code -->
+
+</head>
+<body class="landing">
+
+	<!-- Page Wrapper -->
+	<!-- Header -->
+	<header id="header" class="alt">
+		<nav id="nav">
+			<ul>
+				<li class="special">
+					<a href="#menu" class="menuToggle"><span>Projects</span></a>
+					<div id="menu">
+						<?php include 'menu.php';?>
+					</div>
+				</li>
+			</ul>
+		</nav>
+	</header>
+
+
+	<section id="banner">
+		<div class="inner">
+			<h2>Ben's Spotify Playlist</h2>
+			<!-- PROJECT START -->
+			<div class="container">
+				<iframe src="https://docs.google.com/document/d/1snfKzDqosjOptdPOBoaRxHlyVMIGiOpw3gI_YPU2uBI/pub?embedded=true"></iframe>
+			</div>
+			<!-- / PROJECT END -->
+			<!-- Copyright Signature -->
+			<script>
+				document.write("<p>Copyright © " + new Date().getFullYear() + " <a href='http://www.benmward.com'>Benjamin M. Ward</a></p>");
+			</script>
+		</div>
+	</section>
+
+
+
+	<!-- Scripts -->
+	<script src="assets/js/jquery.min.js"></script>
+	<script src="assets/js/jquery.scrollex.min.js"></script>
+	<script src="assets/js/jquery.scrolly.min.js"></script>
+	<script src="assets/js/skel.min.js"></script>
+	<script src="assets/js/util.js"></script>
+	<script src="assets/js/main.js"></script>
+
+</body>
+</html>
