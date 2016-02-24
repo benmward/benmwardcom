4a1bab1b21ad2ebdb350adc2369aba32f4dfa8b5
Benjamin Ward <benmward41@gmail.com>
2016-02-24 12:57:37 -0500

    minor update

--- menu.php
+++ menu.php
@@ -15,7 +15,7 @@ echo '
 							<li><a href="cataclysm.php">Cataclysm</a></li>
 							<li><a href="tabulater.php">Tabulater</a></li>
 							<hr>
-							<li><a href="seniorcapstone.php">Senior Capstone Project</a></li>
+							<li><a href="seniorcapstone.php">Capstone Project</a></li>
 							<li><a href="hackathons.php">Hackathons Attended</a></li>
 							<li><a href="/childhood">Childhood Works Index</a></li>
 						</ul>
